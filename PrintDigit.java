30644d17b98045394eabf93b4d5a80ccffc4cb0e
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-22 12:51:06 +0530

    Create PrintDigit.java

---
+++ PrintDigit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		String aa=String.valueOf(n);
+		String a[]=aa.split("");
+		for(int i=0;i<a.length;i++){
+			System.out.print(a[i]+" ");
+		}
+}}
